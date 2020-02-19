73b77e204e82daa49d9c1fd7b10da83ff0b5a3db
Zafir Hasan <zafirhsn@gmail.com>
2020-02-19 00:40:09 -0500

    :card_file_box: Configuring MongoDB

--- index.js
+++ index.js
@@ -5,8 +5,28 @@ const fs = require("fs");
 const port = 3000;
 
 
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://zafir:Jackawahoo1!@cluster0-twsfv.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true} );
+
+app.post("/", (req, res, next)=> {
+  console.log("Recieved a post request")
+  client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    const db = client.db("test");
+
+    db.collection("testcol").insertOne({a:100}, (err, r)=> {
+      console.log("inserted data")
+      client.close();
+    })
+  });
+  res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+  res.send("This is a post");
+})
+
+
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:8080");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
@@ -14,6 +34,7 @@ app.use(function(req, res, next) {
 app.get('/', (req, res, next)=> {
   res.send("It Works!");
 })
+
 
 var server = app.listen(3000, function() {
   console.log(`Backend server is listening on port ${port}`);
