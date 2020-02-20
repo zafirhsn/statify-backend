534cc30d9b998547906839b939e7b941336c0d23
Zafir Hasan <zafirhsn@gmail.com>
2020-02-20 00:31:25 -0500

    :card_file_box: Storing data on db

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const fs = require("fs");
+const bodyParser = require('body-parser');
+const assert = require("assert");
+const cors = require("cors");
+
 
 const port = 3000;
 
@@ -8,6 +12,15 @@ const port = 3000;
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://zafir:Jackawahoo1!@cluster0-twsfv.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true} );
+
+app.use(express.json());
+app.use(cors());
+
+
+app.get("/:id", (req, res, next)=> {
+  
+})
+
 
 app.post("/", (req, res, next)=> {
   console.log("Recieved a post request")
@@ -24,6 +37,30 @@ app.post("/", (req, res, next)=> {
   res.send("This is a post");
 })
 
+app.post("/storeuser", (req, res, next)=> {
+  console.log("POST /storeuser", req.body);
+  client.connect(err => {
+    if (err) {
+      console.log("Database connection err: ", err);
+      res.sendStatus(500);
+    }
+    const collection = client.db("statify").collection("users");;
+    collection.updateOne({_id: req.body.id}, {$set: {_id: req.body.id, [req.body.id]: req.body}}, {upsert: true}, (err, resultdoc)=> {
+      if (err) {
+        console.log("Database transaction err: ", err)
+        res.sendStatus(500);
+      } else {
+        // console.log(resultdoc);
+        // assert.strictEqual(r.insertedId, req.body.id);
+        console.log("Document was succesfully inserted");
+        res.sendStatus(200);
+        client.close()
+      }
+    })
+  });
+
+
+});
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:8080");
